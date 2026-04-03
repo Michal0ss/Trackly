a4610da7ee97aacda692caa139e1fb0c6798f275
Michal0ss <michal.bialas.pol@gmail.com>
2026-04-03 18:52:20 +0200

    created tables for db

--- app/database/models.py
+++ app/database/models.py
@@ -1,12 +1,32 @@
-from sqlalchemy import Column, Integer, String, Float, Date
+from datetime import datetime
+
+from sqlalchemy import Boolean, Column, Date, Float, ForeignKey, Integer, String, DateTime
+from sqlalchemy.orm import relationship
+
 from .db import Base
 
 class Subscription(Base):
     __tablename__ = "subscriptions"
 
     id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(Integer, ForeignKey("users.id"))
+
     name = Column(String)
     price = Column(Float)
-    currency = Column(String)
+    currency = Column(String(3))
     billing_cycle = Column(String)
-    next_payment = Column(Date)
+    next_payment_date = Column(DateTime)
+    is_auto_tracked = Column(Boolean, default=True)
+    created_at = Column(DateTime, default=datetime.utcnow)
+
+    user = relationship("Users", back_populates="subscriptions")
+
+class Users(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True)
+    google_id = Column(String, unique=True)
+    email = Column(String, unique=True, nullable=True)
+    created_at = Column(DateTime, default=datetime.utcnow)
+
+    subscriptions = relationship("Subscription", back_populates="user")
