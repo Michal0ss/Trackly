e413e56da189b6060e8d6ad5e6f096e1dd85d6f7
Szymon <spytel0128@gmail.com>
2026-05-28 01:07:04 +0200

    tests dir, security test

---
+++ tests/test_security.py
@@ -0,0 +1,11 @@
+from app.utils.security import hash_password, verify_password
+
+
+def test_password_hashing_and_verification():
+    plain_password = "haslo123"
+
+    hashed = hash_password(plain_password)
+
+    assert hashed != plain_password
+    assert verify_password(plain_password, hashed) is True
+    assert verify_password("haslo12", hashed) is False
